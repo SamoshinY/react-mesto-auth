5691ced32d067bf200899547c5be14b57decb914
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-03-31 00:22:58 +0700

    добавить бургер меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,68 @@
 import logo from "../images/Logo.svg";
+import { useState } from "react";
 
 const Header = ({ onlogOut, userData, loggedIn, onAuth, buttonText }) => {
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const handleBurgerClick = () => {
+    setIsListOpen(!isListOpen);
+  };
+  const Burger = () => {
+    return (
+      <button
+        type="button"
+        className="header__burger"
+        onClick={handleBurgerClick}
+      >
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+      </button>
+    );
+  };
+
+  const CloseList = () => {
+    return (
+      <button
+        onClick={handleBurgerClick}
+        type="button"
+        className="header__burger-close"
+      ></button>
+    );
+  };
+
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Логотип Место" />
       <div className="header__wrap">
-        {loggedIn && <p className="header__email">{userData.email}</p>}
-        {loggedIn && <button className="header__link-text" type="button" onClick={onlogOut}>Выйти</button>}
-        {!loggedIn && <button className="header__link-text" type="button" onClick={onAuth}>{buttonText}</button>}
+        <img className="header__logo" src={logo} alt="Логотип Место" />
+        {!isListOpen && <Burger />}
+        {isListOpen && <CloseList />}
       </div>
+      {isListOpen && (
+        <div className="header__nav">
+          {loggedIn && <p className="header__email">{userData.email}</p>}
+          {loggedIn && (
+            <button
+              className="header__link-text"
+              type="button"
+              onClick={onlogOut}
+            >
+              Выйти
+            </button>
+          )}
+          {!loggedIn && (
+            <button
+              className="header__link-text"
+              type="button"
+              onClick={onAuth}
+            >
+              {buttonText}
+            </button>
+          )}
+        </div>
+      )}
     </header>
   );
-}
+};
 
 export default Header;
